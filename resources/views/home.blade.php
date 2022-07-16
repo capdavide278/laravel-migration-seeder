5e3d79dddd0f5735d053683f5123c495d91c4ce1
Davide <franc.davi00@gmail.com>
2022-07-16 12:42:38 +0200

    inserimento dati

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,7 +6,17 @@
 <main>
     <ul>
         @foreach ($train as $trains)
-            <li>{{$trains->azienda}}</li>
+            <li>
+                {{$trains->id}}, 
+                {{$trains->azienda}}, 
+                {{$trains->stazione_di_partenza}}, 
+                {{$trains->stazione_di_arrivo}}, 
+                {{$trains->orario_di_partenza}}, 
+                {{$trains->orario_di_arrivo}}, 
+                {{$trains->numero_carrozze}}, 
+                {{$trains->in_orario}}
+                {{$trains->cancellato}}
+            </li>
         @endforeach
     </ul>
 </main>
